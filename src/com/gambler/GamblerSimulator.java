47171fd00dd61baffbadd9a2b105cb817223650d
Avinash <Avichauhan111196@gmail.com>
2021-12-29 09:20:10 +0530

    Added : Bet functionality added

--- src/com/gambler/GamblerSimulator.java
+++ src/com/gambler/GamblerSimulator.java
@@ -1,14 +1,40 @@
+/*
+@Author : Avinash Chauhan
+@Date : 29/12/21
+@Problem Statement : Gambler Simulator
+*/
+
 package com.gambler;
 
-public class GamblerSimulator {
+import java.util.Random;
+
+public class GamblerSimulator
+{
+    // Initialization of variable
+    public static final int stake = 100;
+    public static final int bet = 1;
+
+    // Method defined
+     public static void setBet()
+    {
+        Random rn = new Random();
+        int value = rn.nextInt(2);
+        if(value == 1)
+        {
+            System.out.println("Bet won");
+
+        }
+        else
+        {
+            System.out.println("Bet loss");
+        }
+    }
 
     public static void main(String[] args)
     {
+        // Welcome message added
         System.out.println("Welcome to Gambler Simulator");
-
-        int stake = 100;
-        int bet = 1;
-        
+        // Method called
+        setBet();
     }
-
 }
