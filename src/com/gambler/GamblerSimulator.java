69d2e3d66f3dc2eff4efe8d612bf1bbfb783336a
Avinash <Avichauhan111196@gmail.com>
2021-12-29 08:24:58 +0530

    Added : Welcome to Gambler Simulator

---
+++ src/com/gambler/GamblerSimulator.java
@@ -0,0 +1,11 @@
+package com.gambler;
+
+public class GamblerSimulator {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Gambler Simulator");
+
+    }
+
+}
